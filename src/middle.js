0be51afa483b355975ba5d5216383de7cffd9bcb
elias-barbero <elias.barbero@hotmail.com>
2021-08-09 19:58:28 +0200

    fix: inputs

--- src/middle.js
+++ src/middle.js
@@ -10,17 +10,17 @@ const Middle = () => {
   };
 
   const handleBet = (e) => {
-    setBet(e.target.value);
+    console.log('execute smart contract bet');
   };
   console.log(bet);
 
   return (
     <div className='flex justify-center items-center h-2/6 '>
       <div className='w-full px-20'>
-        <div className='w-64 flex justify-between'>
+        <div className='w-64 flex justify-between items-center'>
           <h2>Adversary</h2>
           <input
-            className='border-2 border-gray-200 rounded w-40 py-2 px-4 text-black leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
+            className='m-2 border-2 border-gray-200 rounded w-64 py-2 px-4 text-black leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
             value={address}
             placeholder='Enter Address'
             onChange={(e) => setAddress(e.target.value)}
@@ -56,15 +56,16 @@ const Middle = () => {
           <input
             className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
             type='number'
+            min='0'
             value={bet}
             placeholder='Enter Bet'
-            onChange={(e) => setBet(e.target.value)}
-            //onSubmit
+            onChange={(e) => setBet(e.target.value || 0)}
           />
 
           <button
             className='m-4 flex-shrink-0 bg-green-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded'
             type='button'
+            onClick={handleBet}
           >
             Bet{' '}
           </button>
